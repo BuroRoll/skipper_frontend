ea4cc08fa6f9da8f5cd5957fae8b19ee8de265eb
Danil Konkov <BuroRoll@gmail.com>
2022-04-27 14:17:17 +0500

    change docker

--- src/api/api_setting.js
+++ src/api/api_setting.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import authStore from "../store/authStore";
 
-export let API_URL = 'skipper_backend:8000'
+export let API_URL = 'http://localhost:8000/'
 
 let $api = axios.create({
     baseURL: API_URL
